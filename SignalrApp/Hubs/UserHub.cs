9c5b6f5f48d8aa3a74a9d14ae4501fa8bef0a7f4
bastolamanoj <manojbastola676@gmail.com>
2024-02-19 00:08:07 +0545

    Add project files.

---
+++ SignalrApp/Hubs/UserHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalrApp.Hubs
+{
+    public class UserHub:Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            //send update to all clients that total view have been updated.
+
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
+    }
+}
---
+++ SignalrApp/wwwroot/js/userCount.js
@@ -0,0 +1,33 @@
+﻿"use strict";
+
+//Create Connection
+//var ConnectionUserCount = new signalR.HubConnectionBuilder.WithUrl("/hubs/userCount").Build();
+var ConnectionUserCount = new signalR.HubConnectionBuilder()
+    .withUrl("/hubs/userCount")
+    .build();
+
+// connect to method that hub invokes aka receive notification from hub
+ConnectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewCounter");
+    newCountSpan.innerText = value.toString();
+
+})
+
+// invoke hub method aka send notification to hub
+function newWindowLoadedOnClient() {
+    ConnectionUserCount.send("NewWindowLoaded");
+}
+
+//start connection
+
+function fullfilled() {
+    //do something on start
+    newWindowLoadedOnClient();
+    console.log("Connection to user Hub successful.");
+}
+function rejected() {
+    //rejected logs
+    console.log("Rejected");
+}
+ConnectionUserCount.start().then(fullfilled, rejected);
+   
